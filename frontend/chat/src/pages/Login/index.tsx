8f669721b8315ca8c858979d5cb092d1f0dec50e
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-28 21:30:04 -0300

    ad login page with styled componens

--- frontend/chat/src/pages/Login/index.tsx
+++ frontend/chat/src/pages/Login/index.tsx
@@ -1,5 +1,24 @@
+import logoSvg from '../../assets/images/logo.svg'
+import { Button, Div } from './styles';
+
+
 export function Login() {
     return (
-        <h2>Login</h2>
+        <div className="min-h-screen bg-pGray-200 flex flex-col justify-between items-center">
+            <div className="pt-40"><img src={logoSvg} alt="project-logo" className="mx-auto" /></div>
+            <div className="pb-48 flex flex-col justify-between items-center w-full">
+                <Button.login className="py-5 w-4/5 mx-6">
+                    Login com Google
+                </Button.login>
+                <Div.or className="flex items-center justify-between w-4/5 py-7">
+                    <div className="line grow ml-6"></div>
+                    <span className="px-5">or</span>
+                    <div className="line grow mr-6"></div>
+                </Div.or>
+                <Button.login className="py-5 w-4/5 mx-6">
+                    Login com Apple
+                </Button.login>
+            </div>
+        </div>
     )
 }
---
+++ frontend/chat/src/pages/Login/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+
+
+export const Div = {
+    or: styled.div`
+      .line {
+        height: 1px;
+        background: var(--pBlack-300);
+      }
+      span {
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 17px;
+        color: var(--pBlack-300);
+      }
+    `,
+}
+
+
+export const Button = {
+    login: styled.button`
+      background: #FFFFFF;
+      box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.1);
+      border-radius: 8px;
+      font-weight: 500;
+      font-size: 16px;
+      line-height: 19px;
+      color: var(--pBlack-700);
+    `,
+}
