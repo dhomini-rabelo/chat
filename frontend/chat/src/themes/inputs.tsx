3f417e47a8c988b58e16eee325163c6ab54b390e
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-30 21:12:50 -0300

    up input-form theme

--- frontend/chat/src/themes/inputs.tsx
+++ frontend/chat/src/themes/inputs.tsx
@@ -1,5 +1,8 @@
-export function InputForm({name}: {name: string}) {
+import { InputHTMLAttributes } from "react";
+
+
+export function InputForm({name, moreClasses, ...attributes}: {name: string, moreClasses: string, attributes: InputHTMLAttributes<HTMLInputElement>}) {
     return (
-        <input type="text" name={name} id={name} placeholder="Digite seu username" className="h-14 sm:h-12 d-block mx-auto input-form" />
+        <input type="text" name={name} id={name} placeholder="Digite seu username" className={`h-14 sm:h-12 d-block mx-auto input-form ${moreClasses}`} {...attributes} />
     )
 }
