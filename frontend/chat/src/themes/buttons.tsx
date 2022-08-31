b20b5b041e9fe4cdcebbcb3da05dc0fcdd41375d
dhomini-rabelo <dhominisalgado@gmail.com>
2022-08-30 21:39:55 -0300

    add button-form component

---
+++ frontend/chat/src/themes/buttons.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from "react";
+
+export function ButtonForm({children}: {children: JSX.Element | string}) {
+
+    return (
+        <button className="d-block text-white py-4 mx-auto button-form">
+            {children}
+        </button>
+    )
+}
