6f11499f90bc49d0f5a4fff33eb6fec73c436fe6
dhomini-rabelo <dhominisalgado@gmail.com>
2022-10-02 22:25:54 -0300

    rm hello world route

--- backend/project/apps/accounts/api/__init__.py
+++ backend/project/apps/accounts/api/__init__.py
@@ -1,10 +1,1 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
 from .user import CreateUserApi
-
-@api_view(['GET'])
-def hello_world(request):
-    return Response({
-        "hello": "world"
-    }, status=status.HTTP_200_OK)
--- backend/project/apps/accounts/urls.py
+++ backend/project/apps/accounts/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path('hello', api.hello_world),
     path('create-user', api.CreateUserApi.as_view()),
 ]
