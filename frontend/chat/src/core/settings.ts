12aa374893b1e757eac6bf198b5011805e8ba193
dhomini-rabelo <dhominisalgado@gmail.com>
2022-10-05 20:32:30 -0300

    rm onInvalidSubmit

--- frontend/chat/src/core/settings.ts
+++ frontend/chat/src/core/settings.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 
 
 export const client = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://localhost:8000/api',
 })
--- frontend/chat/src/pages/Register/index.tsx
+++ frontend/chat/src/pages/Register/index.tsx
@@ -1,32 +1,32 @@
 import { Button, Form } from './styles';
 import { InputForm } from '../../themes/inputs';
 import { ButtonForm } from '../../themes/buttons';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { RegisterUserSchemaType, RegisterUserSchema } from '../../code/schemas/user/register';
 import { zodResolver } from '@hookform/resolvers/zod'
 import { FieldErrors, useForm } from 'react-hook-form';
 import { BaseSyntheticEvent } from 'react';
 import { ErrorMessage } from '@hookform/error-message';
+import { client } from '../../core/settings';
 
 
 
 export function Register() {
+    const navigateTo = useNavigate()
     const registerForm = useForm<RegisterUserSchemaType>({
         resolver: zodResolver(RegisterUserSchema),
     })
     const { handleSubmit, formState: { errors }, register } = registerForm
 
 
-    function onValidSubmit(data: RegisterUserSchemaType) {
+    async function onValidSubmit(data: RegisterUserSchemaType) {
 
-    }
 
-    function onInvalidSubmit(errors: FieldErrors<RegisterUserSchemaType>, event?: BaseSyntheticEvent) {
     }
 
 
     return (
-        <Form.container onSubmit={handleSubmit(onValidSubmit, onInvalidSubmit)} className="min-h-screen mx-auto">
+        <Form.container onSubmit={handleSubmit(onValidSubmit)} className="min-h-screen mx-auto">
             <div className="pt-16"><img src="logo.svg" alt="project-logo" className="mx-auto h-12" /></div>
             <h3 className="py-10 text-pBlack-700 text-center"><strong>Cadastro</strong></h3>
             <div className="form-container-df mx-auto">
