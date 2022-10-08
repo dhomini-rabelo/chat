f36e6f6e673201c03440fb08938d7443cd33a0b1
dhomini-rabelo <dhominisalgado@gmail.com>
2022-10-07 21:04:09 -0300

    format files with eslint

--- frontend/chat/src/pages/Login/index.tsx
+++ frontend/chat/src/pages/Login/index.tsx
@@ -1,24 +1,44 @@
-import { Button, Div } from './styles';
-import { InputForm } from './../../themes/inputs';
-import { ButtonForm } from './../../themes/buttons';
-import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
-import { client } from '../../core/settings';
-
+import { Button, Div } from './styles'
+import { InputForm } from './../../themes/inputs'
+import { ButtonForm } from './../../themes/buttons'
+import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { client } from '../../core/settings'
 
 export function Login() {
-    return (
-        <Div.container className="min-h-screen mx-auto">
-            <div className="pt-16"><img src="logo.svg" alt="project-logo" className="mx-auto h-12" /></div>
-            <h3 className="py-10 text-pBlack-700 text-center"><strong>Login</strong></h3>
-            <div className="flex flex-col items-center mb-40 sm:mb-8">
-                <InputForm name="username" placeholder="Digite seu username" />
-                <InputForm name="password" type="password" moreClasses="mt-5" placeholder="Digite sua senha" />
-                <a href="#" className="mt-4 ml-8 forgot self-start"><strong>Forgot password?</strong></a>
-            </div>
-            <div className="flex flex-col items-center"><ButtonForm>Entrar</ButtonForm></div>
-            <span className="block text-center mt-8" id="already">Já cadastrou uma conta ?</span>
-            <Link to="/cadastro" id="register-link" className="block text-center mt-1 text-pBlue-300">Cadastrar-se</Link>
-        </Div.container>
-    )
+  return (
+    <Div.container className="min-h-screen mx-auto">
+      <div className="pt-16">
+        <img src="logo.svg" alt="project-logo" className="mx-auto h-12" />
+      </div>
+      <h3 className="py-10 text-pBlack-700 text-center">
+        <strong>Login</strong>
+      </h3>
+      <div className="flex flex-col items-center mb-40 sm:mb-8">
+        <InputForm name="username" placeholder="Digite seu username" />
+        <InputForm
+          name="password"
+          type="password"
+          moreClasses="mt-5"
+          placeholder="Digite sua senha"
+        />
+        <a href="#" className="mt-4 ml-8 forgot self-start">
+          <strong>Forgot password?</strong>
+        </a>
+      </div>
+      <div className="flex flex-col items-center">
+        <ButtonForm>Entrar</ButtonForm>
+      </div>
+      <span className="block text-center mt-8" id="already">
+        Já cadastrou uma conta ?
+      </span>
+      <Link
+        to="/cadastro"
+        id="register-link"
+        className="block text-center mt-1 text-pBlue-300"
+      >
+        Cadastrar-se
+      </Link>
+    </Div.container>
+  )
 }
