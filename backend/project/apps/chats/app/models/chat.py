baefdb9460a4451ff317c64afcf1e519442555e0
dhomini-rabelo <dhominisalgado@gmail.com>
2022-10-09 15:16:44 -0300

    up code field for unique

--- backend/project/apps/chats/app/models/chat.py
+++ backend/project/apps/chats/app/models/chat.py
@@ -5,7 +5,7 @@ from apps.accounts.app.models import User
 
 class Chat(Model):
     created_by = ForeignKey(User, on_delete=SET_NULL, null=True, related_name='created_chats')
-    code = CharField(max_length=5)
+    code = CharField(max_length=5, unique=True)
     image = CharField(max_length=100000000) # base64
     messages = JSONField() # MessagesType -> apps\chats\actions\models\chat\types.py
     users = ManyToManyField(User, related_name='chats')
