dc5e46a3718ea5d7890c0ac65c91d36dcd98843e
dhomini-rabelo <dhominisalgado@gmail.com>
2022-10-08 21:38:51 -0300

    add chats app

--- backend/project/CHAT/settings.py
+++ backend/project/CHAT/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # My apps
+    'apps.chats.app.ChatsConfig',
     'apps.accounts.app.AccountsConfig',
     # Others apps
     'rest_framework',
---
+++ backend/project/apps/chats/app/__init__.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class ChatsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.chats.app'
+    verbose_name = 'chats'
+    label = 'chats'
