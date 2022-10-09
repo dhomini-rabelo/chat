d91705eb4c48c3f57461c5e3464ab7e1a9722086
dhomini-rabelo <dhominisalgado@gmail.com>
2022-10-09 16:26:58 -0300

    use eslint in pages

--- frontend/chat/src/pages/Actions/index.tsx
+++ frontend/chat/src/pages/Actions/index.tsx
@@ -1,39 +1,45 @@
-import { Button, Div } from './styles';
-import { InputForm } from '../../themes/inputs';
-import { ButtonForm } from '../../themes/buttons';
-import { Link } from 'react-router-dom';
-import { ChatCircleDots, ChatsCircle, SignIn } from 'phosphor-react';
-import { BackButton } from '../../components/BackButton';
-
+import { Div } from './styles'
+import { ButtonForm } from '../../themes/buttons'
+import { Link } from 'react-router-dom'
+import { ChatCircleDots, SignIn } from 'phosphor-react'
+import { BackButton } from '../../components/BackButton'
 
 export function Actions() {
-    return (
-        <Div.container className="min-h-screen mx-auto flex flex-col items-center justify-between">
-            <div className="w-full">
-                <div className="pt-4 w-full">
-                    <BackButton to="/chats" />
-                </div>
-                <div className="pt-12"><img src="logo.svg" alt="project-logo" className="mx-auto h-12" /></div>
-            </div>
+  return (
+    <Div.container className="min-h-screen mx-auto flex flex-col items-center justify-between">
+      <div className="w-full">
+        <div className="pt-4 w-full">
+          <BackButton to="/chats" />
+        </div>
+        <div className="pt-12">
+          <img src="/logo.svg" alt="project-logo" className="mx-auto h-12" />
+        </div>
+      </div>
 
-            <img src="https://images.unsplash.com/photo-1525770041010-2a1233dd8152?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTI4fHxjaGF0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="chat-app-image" className="mx-auto block rounded-full h-64 w-64" />
+      <img
+        src="https://images.unsplash.com/photo-1525770041010-2a1233dd8152?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTI4fHxjaGF0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        alt="chat-app-image"
+        className="mx-auto block rounded-full h-64 w-64"
+      />
 
-            <div className="w-full mb-4">
-                <div className="flex flex-col items-center mb-2">
-                    <ButtonForm>
-                        <Link to="/acoes/criar-sala">
-                            <span><ChatCircleDots size={24} className="inline" /> Criar uma sala</span>
-                        </Link>
-                    </ButtonForm>
-                </div>
-                <div className="flex flex-col items-center">
-                    <ButtonForm>
-                        <Link to="/acoes/entrar-na-sala">
-                            <SignIn size={24} className="inline" /> Entrar uma sala
-                        </Link>
-                    </ButtonForm>
-                </div>
-            </div>
-        </Div.container>
-    )
+      <div className="w-full mb-4">
+        <div className="flex flex-col items-center mb-2">
+          <ButtonForm>
+            <Link to="/acoes/criar-sala">
+              <span>
+                <ChatCircleDots size={24} className="inline" /> Criar um chat
+              </span>
+            </Link>
+          </ButtonForm>
+        </div>
+        <div className="flex flex-col items-center">
+          <ButtonForm>
+            <Link to="/acoes/entrar-na-sala">
+              <SignIn size={24} className="inline" /> Entrar no chat
+            </Link>
+          </ButtonForm>
+        </div>
+      </div>
+    </Div.container>
+  )
 }
--- frontend/chat/src/pages/enterRoom/index.tsx
+++ frontend/chat/src/pages/enterRoom/index.tsx
@@ -1,33 +1,34 @@
-import { Button, Div } from './styles';
-import { InputForm } from '../../themes/inputs';
-import { ButtonForm } from '../../themes/buttons';
-import { Link } from 'react-router-dom';
-import { ChatCircleDots, ChatsCircle, SignIn } from 'phosphor-react';
-import { BackButton } from '../../components/BackButton';
-
-
+import { Div } from './styles'
+import { ButtonForm } from '../../themes/buttons'
+import { SignIn } from 'phosphor-react'
+import { BackButton } from '../../components/BackButton'
 
 export function EnterRoom() {
-    return (
-        <Div.container className="min-h-screen mx-auto flex flex-col items-center justify-between">
-            <div className="w-full">
-                <div className="pt-4 w-full">
-                    <BackButton to="/acoes" />
-                </div>
-                <div className="pt-12"><img src="logo.svg" alt="project-logo" className="mx-auto h-12" /></div>
-            </div>
+  return (
+    <Div.container className="min-h-screen mx-auto flex flex-col items-center justify-between">
+      <div className="w-full">
+        <div className="pt-4 w-full">
+          <BackButton to="/acoes" />
+        </div>
+        <div className="pt-12">
+          <img src="/logo.svg" alt="project-logo" className="mx-auto h-12" />
+        </div>
+      </div>
 
-            <div className="w-full self-center grow mt-48">
-
-                <div className="flex flex-col items-center">
-                    <InputForm name="username" placeholder="Digite o código da sala" moreClasses="mb-5" />
-                    <ButtonForm>
-                        <span>
-                            <SignIn size={24} className="inline" /> Entrar
-                        </span>
-                    </ButtonForm>
-                </div>
-            </div>
-        </Div.container>
-    )
+      <div className="w-full self-center grow mt-48">
+        <div className="flex flex-col items-center form-container-df mx-auto">
+          <input
+            type="text"
+            placeholder="Digite o código do chat"
+            className="w-full h-14 sm:h-12 d-block mx-auto input-form mb-5"
+          />
+          <ButtonForm>
+            <span>
+              <SignIn size={24} className="inline" /> Entrar
+            </span>
+          </ButtonForm>
+        </div>
+      </div>
+    </Div.container>
+  )
 }
